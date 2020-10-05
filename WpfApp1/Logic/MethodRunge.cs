70a0d2e8540d8b359476c67db2ffd5c3adf0f20c
Сафиюлин Александр <rootalfa4@gmail.com>
2020-10-05 22:17:09 +0300

    Cauchi problem solved with first version of graphics

--- WpfApp1/Logic/MethodRunge.cs
+++ WpfApp1/Logic/MethodRunge.cs
@@ -1,76 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
-using System.Windows.Media.TextFormatting;
+using OxyPlot;
 
 namespace WpfApp1.Logic {
     public class MethodRungeCutt {
         public MethodRungeCutt(double                       x,
                                double                       y,
-                               double                       a,
-                               double                       b,
+                               double                       limit,
                                double                       h,
-                               Func<double, double, double> function,
-                               double                       epsilon) {
+                               Func<double, double, double> function) {
             this.Function = function;
             this.X        = x;
-            this.A        = a;
-            this.B        = b;
+            this.Limit    = limit;
             this.Y        = y;
             this.H        = h;
-            this.epsilon  = epsilon;
 
             this.CountFunc = (x, y, h) => y + 0.25 * (this.CountFi0(x, y, h) + 3 * this.CountFi2(x, y, h));
             this.CountFi0  = (x, y, h) => h * this.Function(x,                 y);
             this.CountFi1  = (x, y, h) => h * this.Function(x + h       / 3.0, y + this.CountFi0(x, y, h)       / 3.0);
             this.CountFi2  = (x, y, h) => h * this.Function(x + 2.0 * h / 3.0, y + 2.0 * this.CountFi1(x, y, h) / 3.0);
-
-            //this.CountFunc = (x, y, h) => y + (this.CountFi0(x, y, h) + 2 * this.CountFi1(x, y, h) + 2 * this.CountFi2(x, y, h) + this.CountFi3(x, y, h)) / 6.0;
-            //this.CountFi0 = (x, y, h)  => h * this.Function(x, y);
-            //this.CountFi1 = (x, y, h)  => h * this.Function(x + h / 2.0, this.CountFi0(x, y, h) / 2.0);
-            //this.CountFi2 = (x, y, h)  => h * this.Function(x + h / 2.0, this.CountFi1(x, y, h) / 2.0);
-            //this.CountFi3 = (x, y, h)  => h * this.Function(x + h, this.CountFi2(x, y, h) + h);
         }
 
         public Func<double, double, double>         Function  { get; set; }
         public Func<double, double, double, double> CountFunc { get; }
         public Func<double, double, double, double> CountFi0  { get; }
         public Func<double, double, double, double> CountFi1  { get; }
+        public Func<double, double, double, double> CountFi2  { get; }
 
-        public Func<double, double, double, double> CountFi2 { get; }
-        //public Func<double, double, double, double> CountFi3  { get; }
-
-        public       double X       { get; set; }
-        public       double Y       { get; set; }
-        public       double H       { get; set; }
-        public       double A       { get; set; }
-        public       double B       { get; set; }
-        public       double epsilon { get; set; }
-        public const int    K        = 4;
+        public       double X     { get; set; }
+        public       double Y     { get; set; }
+        public       double H     { get; set; }
+        public       double Limit { get; set; }
+        public const int    K        = 3;
+        public const double epsilon  = 0.001;
         public const double MAX_STEP = 1.0;
-        public const double MIN_STEP = 0.001;
+        public const double MIN_STEP = 0.01;
 
-        public List<List<double>> Execute() {
-            var list = new List<List<double>> {
-                new List<double>(), // x
-                //new List<double>(), // y точное
-                new List<double>(), // y приблизительное
-                //new List<double>()  // погрешность
-            };
+        public List<DataPoint> Execute(bool isAuto = false) {
+            var list = new List<DataPoint>();
 
-            var currentX = this.A;
+            var currentX = this.X;
             var currentY = this.Y;
 
             var currentH = this.H;
             var halfH    = this.H / 2.0;
 
-            list[0].Add(currentX);
-            list[1].Add(currentY);
+            list.Add(new DataPoint(currentX, currentY));
 
-            do {
+            while (this.Limit - currentX >= Math.Pow(10.0, -6.0)) {
                 // Делаем обычный шаг
                 var stepNormH = this.CountFunc(currentX, currentY, currentH);
                 // Делаем половинный шаг
@@ -80,34 +57,39 @@ namespace WpfApp1.Logic {
                                             (Math.Pow(2, MethodRungeCutt.K) - 1));
                 var epsilonHalfH = Math.Abs((stepHalfH - stepNormH) / (Math.Pow(2, MethodRungeCutt.K) - 1));
 
-                if (epsilonHalfH > this.epsilon) {
-                    currentH =  halfH;
-                    halfH    /= 2.0;
-                    continue;
+                if (isAuto) {
+                    if (epsilonHalfH > MethodRungeCutt.epsilon) {
+                        currentH =  halfH;
+                        halfH    /= 2.0;
+                        continue;
+                    }
                 }
 
                 currentX += currentH;
-                currentY =  stepNormH;
+                currentY =  stepHalfH;
 
-                if (epsilonNormH <= this.epsilon) {
-                    halfH    =  currentH;
-                    currentH *= 2.0;
+                list.Add(new DataPoint(currentX, currentY));
+
+                if (isAuto) {
+                    if (epsilonNormH <= MethodRungeCutt.epsilon) {
+                        halfH    =  currentH;
+                        currentH *= 2.0;
+                    }
+
+                    if (currentH > MethodRungeCutt.MAX_STEP) {
+                        currentH = MethodRungeCutt.MAX_STEP;
+                    }
+                    else if (currentH < MethodRungeCutt.MIN_STEP) {
+                        currentH = MethodRungeCutt.MIN_STEP;
+                    }
                 }
 
-                if (currentH > MethodRungeCutt.MAX_STEP) {
-                    currentH = MethodRungeCutt.MAX_STEP;
+                if (this.Limit - currentX < currentH) {
+                    currentH = this.Limit - currentX;
                 }
-                else if (currentH < MethodRungeCutt.MIN_STEP) {
-                    currentH = MethodRungeCutt.MIN_STEP;
-                }
-
-                list[0].Add(currentX);
-                list[1].Add(currentY);
-            } while (currentX < this.B);
+            }
 
             return list;
         }
     }
-}
-
 }
--- WpfApp1/VM/WindowVM.cs
+++ WpfApp1/VM/WindowVM.cs
@@ -5,47 +5,67 @@ using org.mariuszgromada.math.mxparser;
 using OxyPlot;
 using OxyPlot.Series;
 using WpfApp1.Basic;
+using WpfApp1.Logic;
 
 namespace WpfApp1.VM {
     public class WindowVM : BaseVM {
         public WindowVM() {
             CultureInfo.CurrentCulture = new CultureInfo("US-en");
-            var series = new AreaSeries();
-            this.Model = new PlotModel { Title = "YEAH!", Subtitle = "YES!", Series = {series} };
+            this.Model = new PlotModel { Title = "YEAH!", Subtitle = "YES!", Series = { new AreaSeries() } };
         }
 
-        public ICommand Magic => new RelayCommand( obj => {
-            this.Model.Series.Add(this.CreateLine());
-            this.Model.InvalidatePlot(true);
-        });
+        public ICommand Magic =>
+            new RelayCommand(obj => {
+                this.Model.Series.Add(this.CreateIntegralLine());
+                this.Model.Series.Add(this.CreateFunctionLine());
+                this.Model.InvalidatePlot(true);
+            });
 
         public PlotModel Model          { get; set; }
         public string    FunctionString { get; set; }
-        public string    AString        { get; set; }
-        public string    BString        { get; set; }
+        public string    HString        { get; set; }
+        public string    LimitString    { get; set; }
         public string    X0String       { get; set; }
         public string    Y0String       { get; set; }
 
         private Function Function => new Function($"F(x, y) = {this.FunctionString}");
-        private double   A        => Convert.ToDouble(this.AString);
-        private double   B        => Convert.ToDouble(this.BString);
-        private double   X0       => Convert.ToDouble(this.X0String);
-        private double   Y0       => Convert.ToDouble(this.Y0String);
 
-        private LineSeries CreateLine() {
-            var result = new LineSeries();
+        private Func<double, double, double> Func =>
+            (x, y) => new Expression($"F({x}, {y})", this.Function).calculate();
 
-            for (var x = this.A; x <= this.B; x += 0.05) {
-                for (var y = this.A; y <= this.B; y += 0.05) {
-                    var expression = new Expression($"F({x}, {y})", this.Function);
-                    var point      = new DataPoint(x, expression.calculate());
-                    if (!result.Points.Contains(point)) {
-                        result.Points.Add(point);
+        private double H     => Convert.ToDouble(this.HString);
+        private double Limit => Convert.ToDouble(this.LimitString);
+        private double X0    => Convert.ToDouble(this.X0String);
+        private double Y0    => Convert.ToDouble(this.Y0String);
+
+        private LineSeries CreateIntegralLine() {
+            var dotsList = new MethodRungeCutt(this.X0, this.Y0, this.Limit, this.H, this.Func).Execute();
+            var line     = new LineSeries();
+
+            foreach (var dot in dotsList) {
+                line.Points.Add(dot);
+            }
+
+            line.Color = OxyColor.FromRgb(0, 0, 255);
+
+            return line;
+        }
+
+        private LineSeries CreateFunctionLine() {
+            var line = new LineSeries();
+
+            for (var x = this.X0; x <= this.Limit; x += this.H) {
+                for (var y = -50.0; y <= 50.0; y += this.H) {
+                    var point = new DataPoint(x, new Expression($"F({x}, {y})", this.Function).calculate());
+                    if (!line.Points.Contains(point)) {
+                        line.Points.Add(point);
                     }
                 }
             }
 
-            return result;
+            line.Color = OxyColor.FromRgb(0, 255, 0);
+
+            return line;
         }
     }
 }
