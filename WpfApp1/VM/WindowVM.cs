218750e8be8ea782e02e26632d0e593413184a09
Сафиюлин Александр <rootalfa4@gmail.com>
2020-10-11 15:47:20 +0300

    New icon and using fonts

--- WpfApp1/VM/WindowVM.cs
+++ WpfApp1/VM/WindowVM.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using org.mariuszgromada.math.mxparser;
 using OxyPlot;
 using OxyPlot.Series;
